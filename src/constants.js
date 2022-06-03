8bbd663bfb91676c9f71ab27d09b20ac8b0bbef2
Marc Fabre <nds.kram@gmail.com>
2022-06-03 16:49:31 +0200

    Update text uid

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TYPE_OF_ELEMENTS = {
-    text: '5bfe6168-bb34-41cf-9b6d-d7dda8fa5d93',
+    text: 'aeb78b9a-6fb6-4c49-931d-faedcfad67ba',
     select: '1a63e6b3-2e3f-4550-a9f3-366df68e2ca0',
     // checkbox: 'ww-checkbox',
     custom: 'ww-flexbox',
