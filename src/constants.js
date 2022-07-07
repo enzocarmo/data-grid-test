2f8fbcb4ba66f3bb348ce37f1efaee203b473633
Marc Fabre <nds.kram@gmail.com>
2022-07-07 18:02:54 +0200

    Test new select

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const TYPE_OF_ELEMENTS = {
     text: 'aeb78b9a-6fb6-4c49-931d-faedcfad67ba',
-    select: 'b10e81a1-a2b7-4597-8625-60fe0b1722e1',
+    select: '8933a777-335f-457d-a510-8e40ef726385',
     // checkbox: 'ww-checkbox',
     custom: 'ww-flexbox',
 };
